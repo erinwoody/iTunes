bee193101b21473d52525e727176c2a9c148b5dd
Erin Woody <erinelizabethwoody@gmail.com>
2017-08-09 15:01:15 -0500

    added js to get images and search results

--- main.js
+++ main.js
@@ -7,3 +7,31 @@
 // 3. Create your `fetch` request that is called after a submission
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
+
+
+
+
+var searchInput = document.getElementById("searchInput");
+let resultsDisplay = document.querySelector(".results");
+
+
+
+searchInput.addEventListener("keypress", function (e) {
+  if (e.keyCode === 13) {
+    let url = "https://itunes.apple.com/search?term=" + searchInput.value;
+    axios.get(url)
+      .then(function (response) {
+        console.log(response);
+        let results = response.data.results;
+        resultsDisplay.innerHTML = '';
+        for (i = 0; i < results.length; i++) {
+          let data = results[i];
+          const art =
+            `<img src="${data.artworkUrl100}">`
+          resultsDisplay.innerHTML += art;
+        }
+      });
+  } else {
+    return;
+  }
+});
